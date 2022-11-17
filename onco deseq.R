e1e07d033d7811d39fa5f3a27c2663fc42d528b9
Darko Medin <71905959+DarkoMedin@users.noreply.github.com>
2022-11-17 13:49:40 +0100

    The full R code for the tutorial project
    The code contains the following parts of the tutorial.
    1. Installing packages
    2. Loading the data
    3. Data wrangling
    4. Deseq performance
    5. Volcano plots code
    5. Output code

---
+++ onco deseq.R
@@ -0,0 +1,186 @@
+
+#install DESeq2 using Bioconductor
+if (!require('BiocManager', quietly = TRUE))
+  install.packages('BiocManager')
+
+BiocManager::install('DESeq2')
+
+#Install EnhancedVolcano using Bioconductor
+if (!require('BiocManager', quietly = TRUE))
+  install.packages('BiocManager')
+
+BiocManager::install('EnhancedVolcano')
+
+
+library(DESeq2)
+library(EnhancedVolcano)
+#Load the file containing the experiment design
+#'path'=location of the file
+#design <- read.delim('path/E-GEOD-50760-experiment-design.tsv')
+design=E.GEOD.50760.experiment.design
+View(design)
+
+#Load raw counts data
+#'path'=location of the file
+#raw.counts <- read.delim('path/E-GEOD-50760-raw-counts.tsv')
+raw.counts=E.GEOD.50760.raw.counts
+
+
+#This file is important because it contatin columns relating samples with phenotypes
+design=design[1:37,]
+
+
+#Create labels for normal and primary cancer samples
+label1=rep(c('cancer'),18)
+label2=rep(c('normal'),19)
+labels=c(label1,label2)
+
+
+#Load the genetable
+#Select the variables of interest data and add gene names
+dataset=raw.counts[,c('Gene.Name',design$Run)]
+genetable=raw.counts[,design$Run]
+
+
+#Create the metadata dataframe
+Metadata = data.frame(id=design$Run,type=labels)
+
+
+#Perform DESeq analysus
+dds = DESeqDataSetFromMatrix(genetable,Metadata,~type)
+dds <- DESeq(dds)
+
+
+
+#Create DESeq results object using Benjamini-Hochberg correction
+res=results(object = dds, contrast = c('type','cancer','normal'),
+            pAdjustMethod = 'BH',alpha = 0.000001)
+row.names(res)=dataset$Gene.Name
+summary(res)
+
+
+
+#Create DESeq results object using Holmr correction
+res=results(object = dds, contrast = c('type','cancer','normal'),
+            pAdjustMethod = 'holm', alpha = 0.000001)
+row.names(res)=dataset$Gene.Name
+summary(res)
+
+
+
+#Create publication grade volcano plot with marked genes of interest
+EnhancedVolcano(res,
+                lab = dataset$Gene.Name,
+                x = 'log2FoldChange',
+                y = 'pvalue',
+                pCutoff = 10e-5,
+                FCcutoff = 1.333,
+                xlim = c(-5.7, 5.7),
+                ylim = c(0, -log10(10.2e-12)),
+                pointSize = 1.3,
+                labSize = 2.6,
+                title = 'The results',
+                subtitle = 'Differential expression analysis',
+                caption = 'log2fc cutoff=1.333; p value cutof=10e-5',
+                legendPosition = "right",
+                legendLabSize = 14,
+                col = c('lightblue', 'orange', 'blue', 'red2'),
+                colAlpha = 0.6,
+                drawConnectors = TRUE,
+                hline = c(10e-8),
+                widthConnectors = 0.5)
+
+
+#Create publication grade volcanoplot with marked genes of interest
+EnhancedVolcano(res,
+                lab = dataset$Gene.Name,
+                x = 'log2FoldChange',
+                y = 'pvalue',
+                pCutoff = 10e-6,
+                FCcutoff = 3.6,
+                xlim = c(-5.7, 5.7),
+                ylim = c(0, -log10(10.2e-12)),
+                pointSize = 1.3,
+                labSize = 2.6,
+                title = 'The results',
+                subtitle = 'Differential expression analysis',
+                caption = 'log2fc cutoff=1.333; p value cutof=10e-6',
+                legendPosition = "right",
+                legendLabSize = 14,
+                col = c('lightblue', 'orange', 'blue', 'red2'),
+                colAlpha = 0.6,
+                drawConnectors = TRUE,
+                hline = c(10e-8),
+                widthConnectors = 0.5)
+
+
+#Create the final dataframe consisting of ordered deseq results based on log2fc
+resord=as.data.frame(res)
+finaltable=cbind(dataset$Gene.Name, resord)
+finalrable=finaltable[order(finaltable$log2FoldChange),]
+write.table(finaltable, file = 'finaltable.csv', sep = ',',
+            col.names = NA)
+
+
+
+
+#Create DESeq results object using Holmr correction
+res=results(object = dds, contrast = c('type','cancer','normal'),
+            pAdjustMethod = 'holm', alpha = 0.000001)
+row.names(res)=dataset$Gene.Name
+summary(res)
+
+
+
+#Create publication grade volcano plot with marked genes of interest
+EnhancedVolcano(res,
+lab = dataset$Gene.Name,
+x = 'log2FoldChange',
+y = 'pvalue',
+pCutoff = 10e-5,
+FCcutoff = 1.333,
+xlim = c(-5.7, 5.7),
+ylim = c(0, -log10(10.2e-12)),
+pointSize = 1.3,
+labSize = 2.6,
+title = 'The results',
+subtitle = 'Differential expression analysis',
+caption = 'log2fc cutoff=1.333; p value cutof=10e-5',
+legendPosition = "right",
+legendLabSize = 14,
+col = c('lightblue', 'orange', 'blue', 'red2'),
+colAlpha = 0.6,
+drawConnectors = TRUE,
+hline = c(10e-8),
+widthConnectors = 0.5)
+
+
+#Create publication grade volcanoplot with marked genes of interest
+EnhancedVolcano(res,
+lab = dataset$Gene.Name,
+x = 'log2FoldChange',
+y = 'pvalue',
+pCutoff = 10e-6,
+FCcutoff = 3.6,
+xlim = c(-5.7, 5.7),
+ylim = c(0, -log10(10.2e-12)),
+pointSize = 1.3,
+labSize = 2.6,
+title = 'The results',
+subtitle = 'Differential expression analysis',
+caption = 'log2fc cutoff=1.333; p value cutof=10e-6',
+legendPosition = "right",
+legendLabSize = 14,
+col = c('lightblue', 'orange', 'blue', 'red2'),
+colAlpha = 0.6,
+drawConnectors = TRUE,
+hline = c(10e-8),
+widthConnectors = 0.5)
+
+
+#Create the final dataframe consisting of ordered deseq results based on log2fc
+resord=as.data.frame(res)
+finaltable=cbind(dataset$Gene.Name, resord)
+finalrable=finaltable[order(finaltable$log2FoldChange),]
+write.table(finaltable, file = 'finaltable.csv', sep = ',',
+col.names = NA)
